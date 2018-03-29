0218e27275e0b66f863685db01dca9ae0c2b3c37
mac <mac@bzz-MacBook.local>
2018-03-29 23:47:06 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.model.Person;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/9/6.
+ */
+@Controller
+public class HelloController {
+    @RequestMapping(value = {"/index","/"})
+    public String index(Model  model)
+    {
+        Person single = new Person("hyj",21);
+        List<Person> people = new ArrayList<Person>();
+        Person p1 = new Person("dlp",21);
+        Person p2 = new Person("tq",18);
+        Person p3 = new Person("mk",12);
+        people.add(p1);
+        people.add(p2);
+        people.add(p3);
+        model.addAttribute("singlePerson",single);
+        model.addAttribute("people",people);
+        return "index";
+    }
+}
---
+++ src/main/java/com/example/demo/model/Person.java
@@ -0,0 +1,31 @@
+package com.example.demo.model;
+
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+
+    private String name;
+    private Integer age;
+    public Person()  {
+        super();
+    }
+    public Person(String name,Integer gae) {
+        super();
+        this.name=name;
+        this.age=gae;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name=name;
+    }
+    public Integer getAge() {
+        return age;
+    }
+
+    public Integer setAge(Integer age) {
+        return age;
+    }
+}
